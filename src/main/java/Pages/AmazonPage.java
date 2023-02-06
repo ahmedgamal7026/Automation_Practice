1e66354c7438d7bce78a57935f8dce9c4c8afa30
aothman <ahmedgamal7026.ag@gmail.com>
2023-02-06 12:02:01 +0200

    6/2/2023 second commit

--- src/main/java/Pages/AmazonPage.java
+++ src/main/java/Pages/AmazonPage.java
@@ -2,13 +2,14 @@ package Pages;
 
 import java.time.Duration;
 
-import junit.framework.Assert;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class AmazonPage extends PageBase {
 
